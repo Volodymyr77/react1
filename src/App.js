9eee7b1209977c76ffe68617768cb929bb6b1810
Volodymyr77 <gagarinvov@gmail.com>
2020-10-30 23:47:08 +0200

    people_list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import {peoples} from "./data/data";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+let className = 'target';
+
+return (
+    <div className={className}>
+    {
+        peoples.map(people=>{
+            return (<div className={people.name}> {people.name} - {people.age} - {people.car}</div>)
+        })
+    }
     </div>
-  );
+)
 }
 
 export default App;
---
+++ src/data/data.js
@@ -0,0 +1,7 @@
+export  let peoples = [
+     { name: 'Petro', age: 17, car: 'seat' },
+     { name: 'Vasyl', age: 23, car: 'pegoe' },
+     { name: 'Maryn', age: 22, car: 'shkoda' },
+     { name: 'Mykol', age: 25, car: 'mazda' },
+     { name: 'Oksan', age: 22, car: 'VW' }
+];
